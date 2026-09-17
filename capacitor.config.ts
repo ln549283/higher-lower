9b25c03ef9a9323dc7d38e6d70db8ee0e9ce0482
ln549283 <loic.nebonne@gmail.com>
2026-09-17 16:05:53 +0400

    Use Nibylo Games app identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.loic.whichly',
+  appId: 'com.nibylogames.whichly',
   appName: 'Whichly',
   webDir: 'dist',
   bundledWebRuntime: false,
