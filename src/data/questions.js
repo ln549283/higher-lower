68ddeb98ba5b897a474813dc8e7f03fa572d6f8d
ln549283 <loic.nebonne@gmail.com>
2026-09-17 12:35:12 +0400

    feat: add 240-question comparison bank

---
+++ src/data/questions.js
@@ -0,0 +1,70 @@
+const groups = [
+  {
+    category: 'Hauteur des sommets', unit: 'm',
+    items: [['Everest',8849],['K2',8611],['Kangchenjunga',8586],['Lhotse',8516],['Makalu',8485],['Cho Oyu',8188],['Dhaulagiri I',8167],['Manaslu',8163],['Nanga Parbat',8126],['Annapurna I',8091],['Aconcagua',6961],['Denali',6190],['Kilimandjaro',5895],['Mont Elbrouz',5642],['Mont Kenya',5199],['Puncak Jaya',4884],['Mont Blanc',4806],['Cervin',4478],['Mont Fuji',3776],['Mont Kosciuszko',2228]]
+  },
+  {
+    category: 'Longueur des fleuves', unit: 'km',
+    items: [['Nil',6650],['Amazone',6400],['Yangtsé',6300],['Mississippi-Missouri',6275],['Ienisseï',5539],['Fleuve Jaune',5464],['Ob-Irtych',5410],['Paraná',4880],['Congo',4700],['Amour',4444],['Léna',4400],['Mékong',4350],['Mackenzie',4241],['Niger',4184],['Murray-Darling',3672],['Volga',3530],['Danube',2850],['Gange',2525],['Rhin',1233],['Loire',1006]]
+  },
+  {
+    category: 'Superficie des pays', unit: 'km²',
+    items: [['Russie',17098242],['Canada',9984670],['Chine',9596961],['États-Unis',9525067],['Brésil',8515767],['Australie',7692024],['Inde',3287263],['Argentine',2780400],['Kazakhstan',2724900],['Algérie',2381741],['RDC',2344858],['Arabie saoudite',2149690],['Mexique',1964375],['Indonésie',1904569],['Soudan',1886068],['Libye',1759540],['Iran',1648195],['Mongolie',1564116],['Pérou',1285216],['Tchad',1284000]]
+  },
+  {
+    category: 'Superficie des îles', unit: 'km²',
+    items: [['Groenland',2166086],['Nouvelle-Guinée',785753],['Bornéo',748168],['Madagascar',587041],['Île de Baffin',507451],['Sumatra',473481],['Honshu',225800],['Victoria',217291],['Grande-Bretagne',209331],['Ellesmere',196236],['Sulawesi',180681],['Île du Sud (NZ)',145836],['Java',138794],['Île du Nord (NZ)',111583],['Luzon',109965],['Islande',103000],['Mindanao',97530],['Irlande',84421],['Hokkaido',83424],['Hispaniola',76192]]
+  },
+  {
+    category: 'Point de fusion des métaux', unit: '°C',
+    items: [['Mercure',-38.83],['Étain',231.93],['Plomb',327.46],['Zinc',419.53],['Aluminium',660.32],['Argent',961.78],['Or',1064.18],['Cuivre',1084.62],['Nickel',1455],['Fer',1538],['Titane',1668],['Platine',1768.3],['Tungstène',3422]]
+  },
+  {
+    category: 'Vitesse maximale approximative', unit: 'km/h',
+    items: [['Guépard',100],['Cheval',88],['Lion',80],['Lévrier',72],['Lièvre',72],['Kangourou',71],['Autruche',70],['Girafe',60],['Loup',60],['Rhinocéros',50],['Ours brun',48],['Chat domestique',48],['Humain en sprint',45],['Éléphant',40],['Hippopotame',30]]
+  },
+  {
+    category: 'Durée de gestation moyenne', unit: 'jours',
+    items: [['Éléphant',660],['Girafe',450],['Cheval',340],['Vache',283],['Humain',280],['Gorille',257],['Mouton',152],['Chèvre',150],['Cochon',114],['Lion',110],['Tigre',105],['Chat',65],['Chien',63],['Lapin',31],['Souris',20]]
+  },
+  {
+    category: 'Profondeur maximale', unit: 'm',
+    items: [['Océan Pacifique',10984],['Océan Atlantique',8376],['Mer des Caraïbes',7686],['Océan Indien',7258],['Mer Méditerranée',5267],['Mer Rouge',3040],['Mer Noire',2212],['Lac Baïkal',1642],['Lac Tanganyika',1470],['Mer Caspienne',1025],['Lac Supérieur',406],['Lac Léman',310]]
+  },
+  {
+    category: 'Année de première sortie', unit: '',
+    items: [['Premier iPhone',2007],['Premier iPad',2010],['Nintendo Switch',2017],['PlayStation',1994],['PlayStation 2',2000],['Xbox',2001],['Game Boy',1989],['NES',1983],['World Wide Web public',1991],['YouTube',2005],['Wikipedia',2001],['Android',2008],['Windows 95',1995],['Macintosh',1984],['CD audio',1982],['DVD',1996]]
+  }
+]
+
+const offsets = [1, 2, 3, 5, 7]
+const generated = []
+let id = 1
+
+for (const group of groups) {
+  const used = new Set()
+  for (const offset of offsets) {
+    for (let i = 0; i < group.items.length; i++) {
+      const j = (i + offset) % group.items.length
+      if (i === j) continue
+      const key = [Math.min(i, j), Math.max(i, j)].join('-')
+      if (used.has(key)) continue
+      used.add(key)
+      const [leftLabel, leftValue] = group.items[i]
+      const [rightLabel, rightValue] = group.items[j]
+      if (leftValue === rightValue) continue
+      generated.push({
+        id: `q${String(id++).padStart(3, '0')}`,
+        category: group.category,
+        prompt: group.category === 'Année de première sortie' ? 'Lequel est sorti le plus récemment ?' : 'Lequel a la valeur la plus élevée ?',
+        left: { label: leftLabel, value: leftValue },
+        right: { label: rightLabel, value: rightValue },
+        answer: leftValue > rightValue ? 'left' : 'right',
+        unit: group.unit
+      })
+    }
+  }
+}
+
+export const questions = generated.slice(0, 240)
+export default questions
