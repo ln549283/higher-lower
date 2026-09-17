bbb465ae68bc21f401602934ba327debac9c896a
ln549283 <loic.nebonne@gmail.com>
2026-09-17 15:18:00 +0400

    Bump Whichly PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'higher-lower-v1'
+const CACHE_NAME = 'whichly-v1.0.0'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.svg', './icon-512.svg']
 
 self.addEventListener('install', event => {
