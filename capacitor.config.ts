e9e574bceb91da5bf866af7c909abd44479203fc
ln549283 <loic.nebonne@gmail.com>
2026-09-17 12:34:38 +0400

    build: configure Capacitor Android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.loic.highlowstreak',
+  appName: 'Higher Lower Streak',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
